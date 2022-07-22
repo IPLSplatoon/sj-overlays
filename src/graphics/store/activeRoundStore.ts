f3053a5671eecb85d944f9127264fbdf5134b026
inkfarer <inkfarer@inkfarer.com>
2022-07-23 00:46:55 +0300

    Add team color indicators to stats graphic

--- src/graphics/store/activeRoundStore.ts
+++ src/graphics/store/activeRoundStore.ts
@@ -1,17 +1,20 @@
-import { ActiveRound } from 'schemas';
+import { ActiveRound, SwapColorsInternally } from 'schemas';
 import { defineStore } from 'pinia';
 import { DASHBOARD_BUNDLE_NAME } from '../helpers/constants';
 
 const activeRound = nodecg.Replicant<ActiveRound>('activeRound', DASHBOARD_BUNDLE_NAME);
+const swapColorsInternally = nodecg.Replicant<SwapColorsInternally>('swapColorsInternally', DASHBOARD_BUNDLE_NAME);
 
-export const activeRoundReps = [activeRound];
+export const activeRoundReps = [activeRound, swapColorsInternally];
 
 interface ActiveRoundStore {
     activeRound: ActiveRound;
+    swapColorsInternally: SwapColorsInternally;
 }
 
 export const useActiveRoundStore = defineStore('activeRound', {
-    state: () => ({
-        activeRound: null
-    } as ActiveRoundStore)
+    state: (): ActiveRoundStore => ({
+        activeRound: null,
+        swapColorsInternally: null
+    })
 });
