2526fd3fac435e00f975b3d6cf9eb177752cc4ad
inkfarer <inkfarer@inkfarer.com>
2025-06-29 14:40:34 +0300

    Only transition in break graphic content if switching from gameplay

--- src/graphics/helpers/obs.ts
+++ src/graphics/helpers/obs.ts
@@ -22,37 +22,19 @@ export function bindEntranceToTimeline(timeline: gsap.core.Timeline): void {
     );
 }
 
-export function bindEntranceToTimelineGenerator(generator: () => gsap.core.Timeline): void {
-    bindEntranceToFunction(
-        (e: ObsEvent) => {
-            const timeline = generator();
-            if (e.detail.active) {
-                timeline.delay(useGraphicVariableStore().sceneChangeStartDelay).restart(true);
-            } else {
-                timeline.pause(0);
-                timeline.kill();
-            }
-        },
-        (e: ObsEvent) => {
-            if (e.detail.visible) {
-                generator().delay(useGraphicVariableStore().sceneChangeStartDelay).restart(false);
-            }
-        },
-        () => {
-            generator().delay(1).restart(true);
-        }
-    );
-}
-
 type ChangeHandler = (e: ObsEvent) => void;
 export function bindEntranceToFunction(activeChangeHandler: ChangeHandler, visibleChangeHandler?: ChangeHandler, doubleClickHandler?: () => void): void {
     if (window.obsstudio != null) {
         window.addEventListener('obsSourceActiveChanged', activeChangeHandler);
-        window.addEventListener('obsSourceVisibleChanged', visibleChangeHandler ?? activeChangeHandler);
+        if (visibleChangeHandler) {
+            window.addEventListener('obsSourceVisibleChanged', visibleChangeHandler);
+        }
 
         onUnmounted(() => {
             window.removeEventListener('obsSourceActiveChanged', activeChangeHandler);
-            window.removeEventListener('obsSourceVisibleChanged', visibleChangeHandler ?? activeChangeHandler);
+            if (visibleChangeHandler) {
+                window.removeEventListener('obsSourceVisibleChanged', visibleChangeHandler);
+            }
         });
     } else {
         // use the defined double click handler or call the active change handler, saying that the source is active
